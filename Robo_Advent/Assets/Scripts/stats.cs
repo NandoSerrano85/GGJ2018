f705e261c7b2cfe71946a1a5d7499e3b8e8375c9
Serrano, Fernando <fernando.serrano@cbsinteractive.com>
2018-01-26 22:07:55 -0500

    basic stat script

--- Robo_Advent/Assets/Scripts/stats.cs
+++ Robo_Advent/Assets/Scripts/stats.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class stats : MonoBehaviour {
+public class stats {
+	public static int CountPhysical = 0;
+	public static int CountBlaster = 0;
+	public static int CountCharging = 0;
+	public static int CountRunning = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+	public static int [] LevelCalculation(){
 	
-	// Update is called once per frame
-	void Update () {
-		
+	}
+
+	public static int Health{
+		get{ }
+		set{ }
+	}
+
+	public static int Statmina {
+		get{ }
+		set{ }
+	}
+
+	public static int Physical {
+		get{ }
+		set{ }
+	}
+
+	public static int Blaster {
+		get{ }
+		set{ }
+	}
+
+	public static int Charging {
+		get{ }
+		set{ }
+	}
+
+	public static int Running {
+		get{ }
+		set{ }
 	}
 }
